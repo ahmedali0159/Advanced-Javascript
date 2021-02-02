c6a205c0d924a95d94f9ac54b2d0c95ade937676
Ahmed Ali <pirates0159@gmail.com>
2021-02-02 23:55:24 +0600

    value

---
+++ value.js
@@ -0,0 +1,18 @@
+//const name = "solaiman";
+
+// 0 /false
+// "" /false
+//Undefined /false
+// null / false
+// NaN / false
+// false / false
+
+// '0', '' / true
+// [] / true
+let name = false;
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
