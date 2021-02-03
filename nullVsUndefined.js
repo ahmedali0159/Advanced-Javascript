501f7c8d95b641fbeab85df505c2e4cac3334227
Ahmed Ali <pirates0159@gmail.com>
2021-02-03 13:12:06 +0600

    updated

---
+++ nullVsUndefined.js
@@ -0,0 +1,14 @@
+//let pakhi;
+//function add(num1, num2){
+  //  console.log(num1 + num2);
+//}
+
+//function add(num1, num2){
+  //  console.log(num1, num2);
+//}
+
+//const valoManush = {name: "smart dude",phone:458545}
+//console.log(valoManush.gf);
+
+let fun = undefined;
+console.log(fun);
